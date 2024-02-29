844863af9e98a02c8c44a326d3948775ef1f99f4
timpamungkasudemy <timpamungkas.udemy@gmail.com>
2024-02-29 07:23:26 +0700

    Update sentenceCount range in ai-custom-metrics-04.js

--- k6/014-custom-metrics/ai/ai-custom-metrics-04.js
+++ k6/014-custom-metrics/ai/ai-custom-metrics-04.js
@@ -7,9 +7,8 @@ const BASE_URL = 'http://localhost:8888/alphamart';
 let gaugeContentSize = new Gauge('article_content_size');
 
 export default function () {
-    // Randomize the "sentence-count" between 10 and 100
-    let sentenceCount = Math.floor(Math.random() * 91) + 10;
-    sentenceCount = 100;
+    // Randomize the "sentence-count" between 10 and 200
+    let sentenceCount = Math.floor(Math.random() * 191) + 10;
 
     let res = http.get(`${BASE_URL}/api/promotion/article?sentence-count=${sentenceCount}`, {
         headers: { 'Accept': 'application/json' },
